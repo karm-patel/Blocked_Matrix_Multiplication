dc1255e457ce8f6623bf55a2afcaed976da9efde
karm-patel <karmpatel216@gmail.com>
2022-10-09 23:20:29 +0530

    8192 csv uploaded

--- outgen8192.py
+++ outgen8192.py
@@ -5,7 +5,7 @@ import regex as re
 c_files = glob("code/*.c")
 print(c_files)
 
-piles_8192 = [32,64,128]
+piles_8192 = [16,256,512]
 # for file in c_files:    
 #     
 if not os.path.isdir("code8192"):
