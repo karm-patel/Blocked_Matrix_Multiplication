9c74713b1500a851ab0c4dd5de55a67c5ef956d9
karm-patel <karmpatel216@gmail.com>
2022-10-09 16:41:00 +0530

    v2 added

--- matmul.c
+++ matmul.c
@@ -1,35 +1,61 @@
-#include <stdio.h>
+//j,i,k
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<time.h>
 
+int N = 4;
+double A[4][4];
+double array1[4][4];
+double array2[4][4];
+int B = 2;
 
+void init(){
 
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			array1[i][j] = (rand() % 10) +1;
+            printf("")
+		}
+	}
+	
+	
+	for(int i=0; i<N; i++){
+		for(int j=0; j<N; j++){
+			array2[i][j] = (rand() % 10) + 1;
+		}
+	}
+}
+
+void multiplication(){
+for(int i=0; i<N; i=i+B)
+	{	
+		for(int j=0; j<N; j=j+B){
+ 			
+ 			for (int k=0; k < N; k=k+B){
+ 				for(int ii=i; ii<i+B; ii++){
+ 					for(int jj=j; jj < j+B; jj++){
+ 						for(int kk=k; kk < k+B; kk++){
+ 							A[ii][jj] += array1[ii][kk] * array2[kk][jj];
+ 							}
+ 						}
+ 					}
+ 				}
+ 			}
+ 							
+ 	} 
+}
 
 int main(){
-int i, j, k, ii, jj, kk, N, b;
-N = 4;
-b = 2;
-int A[4][4] = {0};
-int B[4][4] = {{1,2,3,4}, {1,2,3,4}, {1,2,3,4}, {1,2,3,4}};
-int C[4][4] = {{4,3,2,1}, {4,3,2,1}, {4,3,2,1}, {4,3,2,1}};
+	
+	time_t t1; 
+    srand((unsigned) time(&t1)); 
 
-for(i=0; i<N; i=i+b){
-    for(j=0; j<N; j=j+b){
-        for (k=0; k < N; k=k+b){
-            for(ii=i; ii<i+b; ii++){
-                for(jj=j; jj < j+b; jj++){
-                    for(kk=k; kk < k+b; kk++){
-                        A[ii][jj] += B[ii][kk] * C[kk][jj];
-                    }
-                }
-            }
-        }
-    }
+	init();	
+	multiplication();
+ 	
+ 	return 0;
 }
 
-for(i=0; i<N; i+=1){
-    for(j=0; j<N; j+=1){
-        printf("%d ", A[i][j]);
-    }
-    printf("\n");
-}
-return 0;
-}
+
+
